b5e757519176c8e555ff4ac587c355870c6f8b62
teng <zhang.ten@husky.neu.edu>
2019-12-05 16:46:58 -0500

    add product details page

--- Dataentitites/JooleUI/Models/Products.cs
+++ Dataentitites/JooleUI/Models/Products.cs
@@ -10,16 +10,33 @@ namespace JooleUI.Models
     {
 
         public int Product_ID { get; set; }
-        public string Manufacturer_Name { get; set; }
         public string Product_Name { get; set; }
         public string Product_Image { get; set; }
         public string Series { get; set; }
         public string Charecteristics { get; set; }
         public string Model { get; set; }
-        public string MountingLocation { get; internal set; }
-        public string ModelYear { get; internal set; }
-        public string Application { get; internal set; }
-        public string UseType { get; internal set; }
+        public int AirFLow { get; set; }
+        public int PowerMax { get; set; }
+        public int PowerMin { get; set; }
+        public int OVMax { get; set; }
+        public int OVMin { get; set; }
+        public int FanSpeedMax { get; set; }
+        public int FanSpeedMin { get; set; }
+        public int MaxSpeedSound { get; set; }
+        public int SweepDiameter { get; set; }
+
+        public string Manufacturer_Name { get; set; }
+        public string Manufacturer_Department { get; set; }
+        public string Manufacturer_Web { get; set; }
+
+        public string UseType { get; set; }
+        public string Application { get; set; }
+        public string MountingLocation { get; set; }
+        public string Accessories { get; set; }
+        public string ModelYear { get; set; }
+
+
+
         public string Object { get; set; }
     }
 }
